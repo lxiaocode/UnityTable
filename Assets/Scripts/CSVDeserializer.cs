ddd089710589005696642b069da792e6ec410b40
芦雨锋 <luyufeng@kingsoft.com>
2023-06-29 17:18:50 +0800

    Update CSVDeserializer.cs

--- Assets/Scripts/CSVDeserializer.cs
+++ Assets/Scripts/CSVDeserializer.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using CsvHelper;
 using CsvHelper.Configuration;
-
+using UnityEngine.Profiling;
 public class CSVDeserializer : MonoBehaviour
 {
-    public string csvFilePath;
+    private string csvFilePath = Application.persistentDataPath + "/buff.csv";
     private Dictionary<string, Buff> dataObjects;// CSV文件路径
 
 
     public void DeserializeCSV()
     {
+        Profiler.BeginSample("DeserializeCSV");
         dataObjects = new Dictionary<string, Buff>();
         using (StreamReader reader = new StreamReader(csvFilePath))
         using (CsvReader csvReader = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
@@ -23,13 +24,15 @@ public class CSVDeserializer : MonoBehaviour
             }
         }
         Debug.Log("DeserializeCSV sucess");
+        Profiler.EndSample();
     }
 
     public void FindDemo()
     {
+        Profiler.BeginSample("FindDemo");
         if (dataObjects == null) DeserializeCSV();
         var dataObject = dataObjects[string.Format("{0}/{1}", 5005, 20)];
         Debug.Log(string.Format("id = {0}, level = {1}, name = {2}, desc = {3}", dataObject.Id, dataObject.Level, dataObject.Name, dataObject.Desc));
-        
+        Profiler.EndSample();
     }
 }
